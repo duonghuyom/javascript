82e14a31864437cf753e8dc03a6bd7361f27bd3f
Duong Huy <huy.nguyen20@onemount.com>
2022-06-07 10:42:50 +0700

    built in function

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,5 +1,19 @@
-var fullName = 'Nguyen Duong Huy'
-var age = "20"
+// var fullName = 'Nguyen Duong Huy'
+// var age = "20"
 
-alert(fullName)
-alert(age)
+// alert(fullName)
+// alert(age)
+
+console.log(123)
+
+var fullName = 'Nguyen Duong Huy'
+
+console.log(fullName)
+
+var warn = 'warning'
+
+console.warn(warn)
+
+var danger = 'danger'
+
+console.error(danger)
