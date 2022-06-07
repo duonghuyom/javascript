ed69649640f37a5e6d306a97cf90754235f0ab1b
Duong Huy <huy.nguyen20@onemount.com>
2022-06-07 17:40:45 +0700

    lesson 1: basic function

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,5 +1,48 @@
-function tinhsum() { 
-    console.log("day la mot function")
+// vi du ve truyen mot parameter vao function
+function printconsolelog(message) { 
+    console.log(message)
 }
 
-tinhsum()
+printconsolelog("hello")
+
+// vi du ve print ra type
+function printtype(type) { 
+    console.log(typeof type)
+}
+
+printtype("huy")
+
+// vi du ve truyen hai parameters vao function
+function print2params(param1, param2){
+    console.log(param1)
+    console.log(param2)
+}
+
+print2params(1, 2)
+
+// vi du trong function khong co params, ket qua tra ve se la mot array
+function arguments() {
+    console.log(arguments)
+}
+
+arguments('arg1', 'arg2', 'arg3')
+
+// vi du su dung ham for of trong function
+function forof() {
+    for (var param of arguments){
+        console.log(param)
+    }
+}
+
+forof("thu1", "thu2", "thu3", "thu4")
+
+// vi du khac su dung arguments, ket qua tra ve se them dau gach ngang
+function arguments2() {
+    var word = ''
+    for (var param of arguments){
+        word += `${param} _`
+    }
+    console.log(word)
+}
+
+arguments2("nguyen", "duong", "huy")
