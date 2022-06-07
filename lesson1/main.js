e247296af8e1fc8e20eaafeab1f5a39c064580c9
Duong Huy <huy.nguyen20@onemount.com>
2022-06-07 15:55:04 +0700

    lesson 1: cac kieu du lieu

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,5 +1,32 @@
-var a = "0"
-var b = 5
+//data type
 
-// them dau + dang truoc mot chuoi thi se bien chuoi thanh so
-console.log(+a + b); //05
+//string
+var a = 2
+var b = 2.5
+
+//string
+var c = "huy"
+var d = 'huy'
+// hai kieu dau nhay deu nhu nhau chi phu thuoc xem minh thich loai nao hon
+
+//boolean
+var isTrue = True //neu them dau nhay vao thi thanh kieu string 'True'
+
+//undefinded
+var name
+
+//null
+var isNull = null //nothing
+
+//symbol
+var sym = Symbol('sym') //unique
+var sym2 = Symbol('sym2')
+
+sym != sym2 // vi hai gia tri nay khac nhau
+
+// function
+var firstfunctio = function() {
+    alert("hi")
+}
+
+//
