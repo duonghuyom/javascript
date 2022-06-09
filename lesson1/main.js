46fa77063faab45bfa6c77b5b28d99a7d2368b05
Duong Huy <huy.nguyen20@onemount.com>
2022-06-09 10:04:09 +0700

    lesson 3: vi du ve mang su dung splice concat

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,54 +1,22 @@
-// array
-// toString de convert tu mang sang chuoi
-var ex1 = [
-    'duong', 
-    'huy',
-    'nguyen'
-]
-
-console.log(ex1.toString()) //duong,huy,nguyen
-
-//join de add them khoang cach, dau phay
-console.log(ex1.join('-')) //duong-huy-nguyen
-
-//pop de lay ra phan tu cuoi mang, tra ve so phan tu trong mang
-console.log(ex1.pop())
-
-//push de them mot phan tu vao mang
-console.log(ex1.push('tom'))
-console.log(ex1)
-
-//shift xoa di phan tu o dau mang, va tra ve gia tri do
-console.log(ex1.shift())
-
-//unshift de them phan tu vao dau mang
-console.log(ex1.unshift('duong'))
-
-//splicing xoa, cat, chen phan tu moi vao mag
-var ex2 = [
-    'ga',
-    'vit',
-    'ngong'
-]
-console.log(ex2.slice(1, 1, "ngan")) //bdau vi tri 1, xoa 1 phan tu, them phan tu vao vi tri do
-
-//concat dung de noi hai mang
-console.log(ex1.concat(ex2))
-
-//slice dung de cat mot so phan tu cua mang
-console.log(ex2)
-console.log(ex2.slice(1, 2))
-
-// mang exercise
-// Viết hàm tại đây
-function getLastElement(animals) {
-    return animals.slice(-1)
+function test(param1, param2) {
+    console.log(`${param1} ${param2}`)
 }
 
+test('huy', 'duong')
 
-// Ví dụ sử dụng
-var animals = ['Monkey', 'Tiger', 'Elephant'];
-var result = getLastElement(animals);
+var test2 = [
+    'ga',
+    'tom'
+]
+var result = test2.concat('huy')
+console.log(result)
+// ['ga', 'tom', 'huy']
 
-console.log(result); // Expected: "Elephant"
-console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
+var test3 = [
+    'xanh',
+    'hong',
+    'tim'
+]
+var result2 = test3.splice(2, 0, 'trang')
+console.log(test3)
+// []
