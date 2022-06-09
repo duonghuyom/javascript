cd4429cf5156a1207248247dc54c361b5a04a01c
Duong Huy <huy.nguyen20@onemount.com>
2022-06-09 14:23:27 +0700

    lesson 3: date + math random

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,25 +1,15 @@
-// object constructor
+//date
+var date = new Date()
 
-function User(firstName, lastName, age) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.getName = function() {
-        return `${firstName} ${lastName}`
-    }
+console.log(date.getFullYear())
+
+var random = Math.random() * 5
+
+console.log(random)
+
+// random mot so tu 1 den 10
+function randomso(value) {
+    return value[Math.floor(Math.random() * value.length)]
 }
 
-User.prototype.uni = "SB"
-User.prototype.getUniName = function(){
-    return this.uni
-}
-
-var user1 = new User('Huy', 'Nguyen', 20)
-user1.hobby = 'gym'
-var user2 = new User('Huy', "Tom", 2)
-user2['school'] = 'vins'
-
-console.log(user1)
-console.log(user2)
-console.log(user2.getUniName())
-console.log(user1.uni)
+console.log(randomso([1,2,3,4,5,6,7,8,9,10]))
