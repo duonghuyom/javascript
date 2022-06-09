a7f50a15c296c9382fec9ea71c145b8ea214497f
Duong Huy <huy.nguyen20@onemount.com>
2022-06-09 14:35:53 +0700

    lesson 3: switch

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,15 +1,15 @@
-//date
-var date = new Date()
+// switch
+var num = 4
 
-console.log(date.getFullYear())
-
-var random = Math.random() * 5
-
-console.log(random)
-
-// random mot so tu 1 den 10
-function randomso(value) {
-    return value[Math.floor(Math.random() * value.length)]
+switch(num) {
+    case 2:
+        console.log('day la so 2')
+    case 3:
+        console.log('day la so 3')
+        break
+    case 4:
+        console.log('day la so 4')
+        break
+    default:
+        console.log("khong la so gi")
 }
-
-console.log(randomso([1,2,3,4,5,6,7,8,9,10]))
