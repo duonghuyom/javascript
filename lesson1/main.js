18aa33bc6d805c297631fa76b4b5d0f31edb0a3e
Duong Huy <huy.nguyen20@onemount.com>
2022-06-11 10:33:20 +0700

    lesson 4: vong lap while/do

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,19 +1,50 @@
-// vong lap for of
-var color = [
-    'do',
-    'xanh',
-    'tim',
-    'vang'
+// vong lap while
+
+var myarray = [
+    'js',
+    'c+',
+    'c++'
 ]
 
-for ( var value of color){
-    console.log(value)
+var i = 0
+while (i < myarray.length){
+    console.log(myarray[i])
+    i++
 }
 
-var info = {
-    name: 'huy',
-    age: 20
+// vong lap do/while
+
+var i = 0
+
+do {
+    i++
+    console.log(i)
+}while (i < 10)
+
+console.log(i)
+
+// break = continue
+// chay tu 1 den 10 chi in ra so chan
+var i = 0
+for(i; i <= 10; i++){
+    if (i%2 !== 0){
+        continue
+    }
+    console.log(i)
 }
 
-console.log(Object.keys(info)) // lay ra mot mang keys
-console.log(Object.values(info)) // lay ra mot mang values
+// vong lap long nhau - nested loop
+var mymang = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+]
+
+for (var i=0; i < mymang.length; i++)
+{
+    console.log(mymang[i])
+    for(var z = 0; z < mymang[i].length; z++)
+    {
+        console.log(mymang[i][z])
+    }
+}
