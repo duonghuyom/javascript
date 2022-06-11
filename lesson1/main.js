6d519bb950d72c0a30af26e2c54aa35ed6a88a36
Duong Huy <huy.nguyen20@onemount.com>
2022-06-11 12:22:06 +0700

    lesson 4: mot so ham nang cao trong array

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,50 +1,56 @@
-// vong lap while
-
-var myarray = [
-    'js',
-    'c+',
-    'c++'
-]
-
-var i = 0
-while (i < myarray.length){
-    console.log(myarray[i])
-    i++
-}
-
-// vong lap do/while
-
-var i = 0
-
-do {
-    i++
-    console.log(i)
-}while (i < 10)
-
-console.log(i)
-
-// break = continue
-// chay tu 1 den 10 chi in ra so chan
-var i = 0
-for(i; i <= 10; i++){
-    if (i%2 !== 0){
-        continue
-    }
-    console.log(i)
-}
-
-// vong lap long nhau - nested loop
-var mymang = [
-    [1, 2],
-    [3, 4],
-    [5, 6]
-]
-
-for (var i=0; i < mymang.length; i++)
-{
-    console.log(mymang[i])
-    for(var z = 0; z < mymang[i].length; z++)
+// Mang advanced
+var contacts = [
     {
-        console.log(mymang[i][z])
-    }
-}
+        id: 1,
+        name: 'Huy',
+        so: 0
+    },
+    {
+        id: 2,
+        name: 'Duong',
+        so: 9
+    },
+    {
+        id: 3,
+        name: 'Tom',
+        so: 3
+    },
+    {
+        id: 4,
+        name: 'Tom',
+        so: 10
+    },
+]
+
+//forEach() in ra cac phan tu
+var contact = contacts.forEach(function(contact){
+    console.log(contact)
+})
+
+//every() tim xem so moi nguoi co phai 0 ko
+var is0 = contacts.every(function(contact){
+    return contact.so === 0
+})
+
+console.log(is0)
+
+//some() chi can mot phan tu thoa man thi se dung
+var some = contacts.some(function(contact){
+    return contact.so === 0
+})
+
+console.log(some)
+
+//find() tim phan tu theo gia tri yeu cau, chi tra ve mot phan tu tim dc dau tien
+var find = contacts.find(function(contact){
+    return contact.name === "Huy"
+})
+
+console.log(find)
+ 
+//filter() tim phan tu theo gia tri yeu cau, tra lai tat ca phan thu thoa man
+var filter = contacts.filter(function(contact){
+    return contact.name === "Tom"
+})
+
+console.log(filter)
