0a6698a301bc2008eb5f7bb9262672840af66fd4
Duong Huy <huy.nguyen20@onemount.com>
2022-06-11 09:03:28 +0700

    lesson 3: vong lap for 2

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,22 +1,25 @@
-// function getRandNumbers(min, max, length){
-//     var num = []
-//     for (var i = 0; i < length; i++){
-//         num[i] = Math.round(Math.random() * (max - min) + min)
-//     }
-//     return num
-// }
-// getRandNumbers(1, 10, 5)
+// generate ra nhung so random trong mot khoang
+function getRandNumbers(min, max, length){
+    var num = []
+    for (var i = 0; i < length; i++){
+        num[i] = Math.round(Math.random() * (max - min) + min)
+    }
+    return num
+}
+getRandNumbers(1, 10, 5)
 
-// function getTotal(arr) {
-//     var total = 0
-//     for(var i = 0; i < arr.length; i++){
-//         total += arr[i]
-//     }
-//     return total
-// }
+// tinh tong cac so duoc cho
+function getTotal(arr) {
+    var total = 0
+    for(var i = 0; i < arr.length; i++){
+        total += arr[i]
+    }
+    return total
+}
 
-// console.log(getTotal([1,2,3,4]))
+console.log(getTotal([1,2,3,4]))
 
+// tinh tong gia tien cac order
 var orders = [
     {
         name: 'Khóa học HTML - CSS Pro',
