542304f7ba57277434932872cad25d82cdc365f9
Duong Huy <huy.nguyen20@onemount.com>
2022-06-14 10:43:49 +0700

    lesson 5: callback()

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,48 +1,26 @@
-// Array reduce() continue
+// callback()
+// la ham
+// duoc truyen qua doi so
 
-//flat ca mang ben duoi
-var listNum = [1, 2, [3, 4], 5, 6, [7, 8, 9]]
+function myFunction(param){
+    param('OM')
+}
 
-var flatten = listNum.reduce(function(list, num){ // su dung ham reduce de lay ra tung gia tri trong mang
-    return list.concat(num) //concat de noi lai cac gia tri trong mang
-}, []) // initial value o day la mot mang rong de luu cac gia tri vao ben trong
+function callBack(value){
+    console.log('Day la:', value)
+}
 
-console.log(flatten)
+myFunction(callBack)
 
-//lay ra mot array trong mot object su dung reduce()
-var hocSinh = [
-    {
-        name: 'Huy',
-        monHoc: [
-            {
-                id: 1,
-                mon: 'Toan'
-            },
-            {
-                id: 2,
-                mon: 'Van'
-            }
-
-        ]
-    },
-    {
-        name: 'Tom',
-        monHoc: [
-            {
-                id: 3,
-                mon: 'Anh'
-            },
-            {
-                id: 4,
-                mon: 'Ly'
-            }
-
-        ]
-    }
+//
+var courses = [
+    'JS',
+    'C',
+    'Python'
 ]
 
-var monHoc = hocSinh.reduce(function(id, mon){
-    return id.concat(mon.monHoc)
-}, [])
+var htmls = courses.map(function(course){
+    return `<h2>${course}</h2>`
+})
 
-console.log(monHoc)
+console.log(htmls.join(''))
