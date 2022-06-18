fad587b81898e2e107c5cb2c9e113e94911ff201
duonghuyom <huy.nguyen20@onemount.com>
2022-06-18 12:07:07 +0700

    lesson 7: es6

--- lesson1/main.js
+++ lesson1/main.js
@@ -1,14 +1,69 @@
-import fetch from "node-fetch";
-// fetch() dung de goi api va tra ra du lieu
-var api = "https://jsonplaceholder.typicode.com/posts"
+// arrow function
+// goi mot function trong mot object
+const course = {
+    name: 'Javascript basic',
+    getName: function() {
+        return this.name //context, arrow function khong co context
+    }
+}
 
-fetch(api)
-    .then(function(response){
-        return response.json(); // goi api tra ve gia tri json convert sang javascript type
-    })
-    .then(function(posts){
-        console.log(posts)
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+console.log(course.getName())
+
+//khoi tao mot class
+
+class lopHoc {
+    constructor(mon, thay){
+        this.mon = mon,
+        this.thay= thay
+    }
+}
+
+const lopToan = new lopHoc("toan", "tom")
+const lopVan = new lopHoc("van", "huy")
+
+console.log(lopToan)
+console.log(lopVan)
+
+//destructering
+//array
+var array = ['nguyen', 'duong', 'huy']
+
+var [a, b, c] = array
+
+console.log(a, b, c) //typeof string
+
+var [a, ...rest] = array //rest parameter
+console.log(rest) // array
+
+//object
+var nguoi = {
+    ten: 'huy',
+    tuoi: 21
+}
+
+var {ten, tuoi} = nguoi
+
+console.log(ten, tuoi) //string
+
+//...rest
+function test(...params){
+    console.log(params)
+}
+
+test(1,2,3,4, 'huy', 'tom')
+
+//...spread
+var so1 = {
+    ten: 'huy'
+}
+
+var so2 = {
+    ho: 'nguyen'
+}
+
+var so3 = {
+    ...so1,
+    ...so2
+}
+
+console.log(so3)
